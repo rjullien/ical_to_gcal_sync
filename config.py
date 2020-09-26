b3f88a89f7e01a247648cfd6809989cdef0183e2
Rene Jullien <rene.jullien@gmail.com>
2020-09-26 21:43:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 # The iCal feed URL for the events that should be synced to the Google Calendar.
 # Note that the syncing is one-way only.
-ICAL_FEED = '<ICAL FEED URL>'
+ICAL_FEED = 'Not used, replaced by phelma calendar'
 
 # the ID of the calendar to use for iCal events, should be of the form
 # 'ID@group.calendar.google.com', check the calendar settings page to find it.
 # (can also be 'primary' to use the default calendar)
-CALENDAR_ID = '<GOOGLE CALENDAR ID>'
+# Prod
+CALENDAR_ID = 'primary'
 
 # must use the OAuth scope that allows write access
 SCOPES = 'https://www.googleapis.com/auth/calendar'
@@ -17,7 +18,7 @@ CLIENT_SECRET_FILE = 'ical_to_gcal_sync_client_secret.json'
 CREDENTIAL_PATH = 'ical_to_gcal_sync.pckl'
 
 # Application name for the Google Calendar API
-APPLICATION_NAME = 'ical_to_gcal_sync'
+APPLICATION_NAME = 'Phelma calendar'
 
 # File to use for logging output
 LOGFILE = 'ical_to_gcal_sync_log.txt'
