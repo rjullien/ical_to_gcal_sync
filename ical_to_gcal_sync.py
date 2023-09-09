183d9316f70ea459a0c614b042b0b5da2eb03400
Rene Jullien <rene.jullien@gmail.com>
2023-09-09 16:25:18 +0200

    add directory where chrome will download ics by default

--- ical_to_gcal_sync.py
+++ ical_to_gcal_sync.py
@@ -252,8 +252,8 @@ if __name__ == '__main__':
             if feed.get('school') == "ENAC":
                 logger.info('Retrieving events from enac website')
                 month_to_sync = int(config['GCAL_DAYS_TO_SYNC']) // 30
-                logger.info(f"get_enac_ics('{feed['source']}', '{feed['url']}', '{config['ICAL_FEED_USER']}', '{config['ICAL_FEED_PASS']}', {month_to_sync})")
-                # get_enac_ics(feed['source'],feed['ulr'],config['ICAL_FEED_USER'],config['ICAL_FEED_PASS'],month_to_sync) # get ics files from enac website
+                logger.info(f"get_enac_ics('{feed['download']}' ,'{feed['source']}', '{feed['url']}', '{config['ICAL_FEED_USER']}', '{config['ICAL_FEED_PASS']}', {month_to_sync})")
+                # get_enac_ics(feed['download'],feed['source'],feed['ulr'],config['ICAL_FEED_USER'],config['ICAL_FEED_PASS'],month_to_sync) # get ics files from enac website
                 # Should use credentials to get ics files from config['ICAL_FEED_USER'] and config['ICAL_FEED_PASS'] and config['ICAL_FEED_URL'] and config['GCAL_DAYS_TO_SYNC']/30
                 # ICAL_FEED_USER = None
                 # ICAL_FEED_PASS = None
