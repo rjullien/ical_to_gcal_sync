20f927eeece50de37ce1b810e007029f28fb64a6
Rene Jullien <rene.jullien@gmail.com>
2023-09-17 21:25:29 +0200

    support english setup, manage errors in download

--- enac.py
+++ enac.py
@@ -8,7 +8,9 @@ def get_enac_ics(downloadPath, icsPath, url, user, password, nombre_de_mois):
     import os
 
     #nous devrions vérifier si le fichier existe ou non avant de le supprimer.
+    print("processing", nombre_de_mois, "month" if nombre_de_mois > 1 else "months")
     if os.path.exists(icsPath + "/planning.ics"):
+        print("Suppression du fichier: ", icsPath + "/planning.ics")
         os.remove(icsPath + "/planning.ics")
     else:
         print("Impossible de supprimer le fichier planning.ics car il n'existe pas")
@@ -19,6 +21,8 @@ def get_enac_ics(downloadPath, icsPath, url, user, password, nombre_de_mois):
             print(f"Impossible de supprimer le fichier planning ({i}).ics car il n'existe pas")
     chrome_options = Options()
     chrome_options.add_argument('--headless=new')  # Run browser in headless mode
+    #browser_locale = 'fr'
+    #chrome_options.add_argument("--lang={}".format(browser_locale))
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
     driver.find_element(By.ID, "username").send_keys(user)
@@ -29,20 +33,27 @@ def get_enac_ics(downloadPath, icsPath, url, user, password, nombre_de_mois):
     driver.find_element(By.ID, "j_idt28").click()
     driver.implicitly_wait(10)
     sleep(2)
-    driver.find_element(By.LINK_TEXT, "Mon emploi du temps").click()
+    try:
+        driver.find_element(By.LINK_TEXT, "Mon emploi du temps").click()
+    except:
+        driver.find_element(By.LINK_TEXT, "My schedule").click()
     driver.implicitly_wait(100)
     sleep(2)
     driver.find_element(By.XPATH, "//button[@class='fc-month-button ui-button ui-state-default ui-corner-left ui-corner-right']").click()
-    sleep(2)
+    sleep(5)
     driver.find_element(By.ID, "form:j_idt121").click()
     sleep(2)
     for _ in range(1,nombre_de_mois):
-        driver.find_element(By.XPATH, '//body').send_keys(Keys.CONTROL + Keys.HOME)
-        sleep(2)
-        driver.find_element(By.XPATH, "//button[@class='fc-next-button ui-button ui-state-default ui-corner-left ui-corner-right']").click()
-        sleep(2)
-        driver.find_element(By.ID, "form:j_idt121").click()
-        sleep(2)
+        try:
+            print("Téléchargement du planning pour le mois: ", _)
+            driver.find_element(By.XPATH, '//body').send_keys(Keys.CONTROL + Keys.HOME)
+            sleep(5)
+            driver.find_element(By.XPATH, "//button[@class='fc-next-button ui-button ui-state-default ui-corner-left ui-corner-right']").click()
+            sleep(5)
+            driver.find_element(By.ID, "form:j_idt121").click()
+            sleep(5)
+        except:
+            print("Erreur lors du téléchargement du planning pour le mois: ", _)
     sleep(2)
 
     #cut paste the ics file
